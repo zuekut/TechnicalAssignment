afbd65575a79d3385e5a67717ef722cc065229f3
Dimitar Aleksov <daleksov@vitalhealthsoftware.com>
2023-07-17 23:11:19 +0200

    Added HttpClient resilience to Gleif HttpClient requests

--- src/DataSetEnricher/Program.cs
+++ src/DataSetEnricher/Program.cs
@@ -23,6 +23,7 @@ Log.Logger = new LoggerConfiguration()
     .ReadFrom.Configuration(configuration)
     .CreateLogger();
 builder.Host.UseSerilog();
+builder.Services.AddPolicyRegistries();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddGleiHttpClient(gleiApiConfiguration);
